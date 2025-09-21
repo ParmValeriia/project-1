76f9ee6817db8065a1ebae4956029548b1ef587e
ParmValeriia <parmenovavaleria@gmail.com>
2025-09-21 15:36:25 +0300

    parm

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let express = require ("express")
-
+let ads = []
 let app = express()
 app.use(express.static("static"))
-
 app.use(express.json())
+app.set("view engine", "ejs")
+app.set("views", "views")
+
 app.get("/",(req,res)=>{
-    res.send("Hi")
+    res.render("index", {products: ads})
 })
 
 app.post("/add",(req,res)=>{
@@ -17,5 +19,8 @@ app.get("/ads", (req,res)=>{
     res.json(ads)
 })
 
-
+app.use( (req,res, next)=>{
+    res.status = 404
+    res.render("notFound")
+})
 app.listen(3000, ()=>console.log("Server on!"))
--- static/script.js
+++ static/script.js
@@ -18,4 +18,15 @@ document.querySelector(".closeBtn").addEventListener("click", ()=>{
 
 document.querySelector("#openBtn").addEventListener("click", ()=>{
     document.querySelector(".modal").style.display="grid"
+    console.log("dfsdf")
 })
+
+// fetch("/ads").then(res=>res.json()).then(data=>{
+//     console.log(data)
+//     document.querySelector(".wrapper").innerHTML = data.map(el=>`
+//         <div class="/ads">
+//         <h3>${el.title}</h3>
+//         <p>${el.desc}</p>
+//         </div>
+//         `).join("")
+// })
